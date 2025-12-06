69d5ace2b224b74acd1fef255f448d32c39e599f
Bolormaa335 <2025335@student.cct.ie>
2025-12-06 18:06:16 +0000

    empty

--- src/git_2025335/Git_2025335.java
+++ src/git_2025335/Git_2025335.java
@@ -15,6 +15,7 @@ public class Git_2025335 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Bolormaa - 2025335");
 
     }
     
